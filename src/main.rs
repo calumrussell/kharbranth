8223849f77749a42f3552bf3f01fd191f3e88e74
calum <calum.mj.russell@gmail.com>
2025-05-28 21:27:09 +0100

    added ReadHooks, added pong timeout

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,37 @@
-use std::{collections::HashMap, sync::Arc, time::Duration};
+use std::{collections::HashMap, sync::Arc, time::{Duration, SystemTime}};
 
-use anyhow::Result;
 use futures_util::{stream::{SplitSink, SplitStream}, SinkExt, StreamExt};
 use tokio::{net::TcpStream, sync::{broadcast, Mutex}, task::JoinHandle, time::sleep};
-use tokio_tungstenite::{connect_async, tungstenite::{client::IntoClientRequest, Message, Utf8Bytes}, MaybeTlsStream, WebSocketStream};
-use tokio_util::sync::CancellationToken;
+use tokio_tungstenite::{connect_async, tungstenite::{client::IntoClientRequest, protocol::{frame::Frame, CloseFrame}, Message, Utf8Bytes}, MaybeTlsStream, WebSocketStream};
+use tokio_util::{bytes::Bytes, sync::CancellationToken};
 
 type WsStream = WebSocketStream<MaybeTlsStream<TcpStream>>;
 type WsSink = SplitSink<WsStream, Message>;
 type WsSource = SplitStream<WsStream>;
 
-struct ReadHooks {
-    pub on_text: Arc<dyn Fn(Utf8Bytes) + Send + Sync>,
+pub struct ReadHooks {
+    pub on_text: Arc<Option<Box<dyn Fn(Utf8Bytes) + Send + Sync>>>,
+    pub on_binary: Arc<Option<Box<dyn Fn(Bytes) + Send + Sync>>>,
+    pub on_ping: Arc<Option<Box<dyn Fn(Bytes) + Send + Sync>>>,
+    pub on_pong: Arc<Option<Box<dyn Fn(Bytes) + Send + Sync>>>,
+    pub on_close: Arc<Option<Box<dyn Fn(Option<CloseFrame>) + Send + Sync>>>,
+    pub on_frame: Arc<Option<Box<dyn Fn(Frame) + Send + Sync>>>,
 }
 
-struct Connection {
+impl ReadHooks {
+    pub fn new() -> Self {
+        Self {
+            on_text: Arc::new(None),
+            on_binary: Arc::new(None),
+            on_ping: Arc::new(None),
+            on_pong: Arc::new(None),
+            on_close: Arc::new(None),
+            on_frame: Arc::new(None),
+        }
+    }
+}
+
+pub struct Connection {
     pub config: Config,
     pub read: Option<Arc<Mutex<WsSource>>>,
     pub write: Option<Arc<Mutex<WsSink>>>,
@@ -28,16 +45,18 @@ impl Connection {
         let _ = write_lock.send(msg).await;
     }
     
-    async fn ping_loop(&mut self, ping_message: String, ping_duration: u64) -> JoinHandle<()> {
+    async fn ping_loop(&mut self) -> JoinHandle<()> {
         let write_clone = Arc::clone(&self.write.as_ref().unwrap());
+        let ping_message_clone = self.config.ping_message.clone().into_bytes();
+        let ping_duration_clone = self.config.ping_duration.clone();
+
         tokio::spawn(async move {
-            let ping_message_clone = ping_message.into_bytes();
             loop {
                 {
                     let mut write_lock = write_clone.lock().await;
                     let _ = write_lock.send(Message::Ping(ping_message_clone.clone().into())).await;
                 }
-                sleep(Duration::from_secs(ping_duration.into())).await;
+                sleep(Duration::from_secs(ping_duration_clone.into())).await;
             }
         })
     }
@@ -45,17 +64,57 @@ impl Connection {
     async fn read_loop(&mut self, cancel_token: CancellationToken) -> JoinHandle<()> {
         let read_clone = Arc::clone(&self.read.as_ref().unwrap());
         let cancel_token_clone = cancel_token.clone();
-        let callback_clone = Arc::clone(&self.hooks.on_text);
+        let ping_duration_clone = self.config.ping_duration.clone();
+        let mut last_pong = SystemTime::now();
+
+        let on_text_clone = Arc::clone(&self.hooks.on_text);
+        let on_binary_clone = Arc::clone(&self.hooks.on_binary);
+        let on_ping_clone = Arc::clone(&self.hooks.on_ping);
+        let on_pong_clone = Arc::clone(&self.hooks.on_pong);
+        let on_close_clone = Arc::clone(&self.hooks.on_close);
+        let on_frame_clone = Arc::clone(&self.hooks.on_frame);
         tokio::spawn(async move {
             loop {
+                if SystemTime::now() > last_pong.checked_add(Duration::from_secs(ping_duration_clone)).unwrap() {
+                    cancel_token_clone.cancel();
+                }
+
                 let mut read_lock = read_clone.lock().await;
                 if let Some(received) = read_lock.next().await {
                     if let Ok(msg) = received {
                         match msg {
                             Message::Text(text) => {
-                                callback_clone(text);
+                                if let Some(on_text_func) = on_text_clone.as_ref() {
+                                    on_text_func(text);
+                                }
+                            },
+                            Message::Binary(binary) => {
+                                if let Some(on_binary_func) = on_binary_clone.as_ref() {
+                                    on_binary_func(binary);
+                                }
+                            },
+                            Message::Ping(ping) => {
+                                if let Some(on_ping_func) = on_ping_clone.as_ref() {
+                                    on_ping_func(ping);
+                                }
+                            },
+                            Message::Pong(pong) => {
+                                if let Some(on_pong_func) = on_pong_clone.as_ref() {
+                                    on_pong_func(pong);
+                                }
+                                last_pong = SystemTime::now();
+                            },
+                            Message::Close(maybe_frame) => {
+                                if let Some (on_close_func) = on_close_clone.as_ref() {
+                                    on_close_func(maybe_frame);
+                                }
+                                cancel_token_clone.cancel();
+                            },
+                            Message::Frame(frame) => {
+                                if let Some(on_frame_func) = on_frame_clone.as_ref() {
+                                    on_frame_func(frame);
+                                }
                             }
-                            _ => (),
                         }
 
                     }
@@ -73,7 +132,7 @@ impl Connection {
         self.write = Some(Arc::new(Mutex::new(write)));
      
         let read_loop = self.read_loop(cancel_token.clone()).await;
-        let ping_loop = self.ping_loop(self.config.ping_message.to_string(), self.config.ping_duration).await;
+        let ping_loop = self.ping_loop().await;
         
         (read_loop, ping_loop)
     }
@@ -88,13 +147,13 @@ impl Connection {
     }
 }
 
-struct Config {
+pub struct Config {
     pub url: String,
     pub ping_duration: u64,
     pub ping_message: String,
 }
 
-struct WSManager {
+pub struct WSManager {
     conn: HashMap<String, Arc<Mutex<Connection>>>
 }
 
@@ -194,12 +253,11 @@ async fn main() {
         ping_message: "{\"method\": \"ping\"}".to_string(),
     };
     
-    let hooks = ReadHooks {
-        on_text: Arc::new(|text: Utf8Bytes| {
-            println!("{:?}", text);
-        })
-    };
-    
+    let mut hooks = ReadHooks::new();
+    hooks.on_text = Arc::new(Some(Box::new(|text| {
+        println!("{:?}", text);
+    })));
+       
     let mut mgr = WSManager::new();
     mgr.new_conn("hl", hl_config, hooks);
     
